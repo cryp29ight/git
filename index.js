56c16aeac06ba83e95bc128bb204e08b3497b3ae
Spid3r <faridaaa58@gmail.com>
2021-12-27 20:17:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Object.values
+
+const object = {
+    name: 'Spider',
+    ge: 20,
+    favoriteBooks: ['Harry Potter', 'A brief story of time']
+};
+// console.log(Object.values(object));
+
+// Object.entries
+
+// console.log(Object.entries(object));
+
+
+// Async Functions
+
+
+
+// Exponentiations
+
+// console.log(2 ** 3);
+
+
+// Trailing commas
+
+const anotherObject = {
+    first: 1,
+    second: 2,
+    third: 3
+}
+
+// console.log(anotherObject)
